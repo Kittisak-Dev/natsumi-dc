3d6c2f9da268b56dc404aba2ebd4858a9f059aa7
Kittisak-Dev <allahkillboy@gmail.com>
2022-01-14 15:48:07 +0700

    wakeup natsumi

---
+++ push.sh
@@ -0,0 +1,16 @@
+echo "[LOG] Executing bash script"
+
+source .env
+
+git add "."
+git config --local user.name "$name"
+git config --local user.email "$mail"
+
+echo "[LOG] Committing"
+
+read -p "commit message: " msgInput
+
+git commit --author="$name <$mail>" -m "$msgInput"
+git push -u origin "main"
+
+echo "[LOG] Successfully committed and pushed"
---
+++ src/structures/Client.js
@@ -0,0 +1,29 @@
+const { Client: DJS } = require(`discord.js`);
+
+require(`dotenv`).config();
+
+class Client extends DJS {
+    constructor() {
+        super({
+            intents: ['GUILDS', 'GUILD_MESSAGES'],
+        });
+        /** @private */
+        this.mode =
+            process.env.MODE === `development` ? `development` : `production`;
+    }
+    online() {
+        console.clear;
+        this.login(
+            this.mode === 'development'
+                ? `${process.env.DEV_TOKEN}`
+                : `${process.env.TOKEN}`
+        )
+            .catch((err) => {
+                console.log(err.message);
+                process.exit(1);
+            })
+            .finally(() => console.log(`Client Mode :`, this.mode));
+    }
+}
+
+module.exports = { Client };
